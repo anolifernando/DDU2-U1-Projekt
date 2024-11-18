7ba72982bfa74982b5693af2bf34ef751a07d1f3
anolifernando <anolifernando@gmail.com>
2024-11-18 11:34:35 +0100

    påbörjat promt och loop

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Recommended: All functions declared here
+const cityTitle = document.querySelector("h2");
+const titleHead = document.quertSelector("title");
 
 // Recommended: constants with references to existing HTML-elements
 
 const cityName = prompt("Vilken stad?");
-let cityNotFound = false; 
+let cityNotFound = false;
 
+for (let citiesKey in cities) {
+  if (enterCity == cities[citiesKey].name) {
+    cityWasFound = true;
+    cityTitle.innerHTML = `${enterCity} (${cities[citiesKey].country})`;
 
-for (let citiesKey)
+    //break;
+  }
+}
 
 // Recommended: Ask for the city name and then the rest of the code
-
