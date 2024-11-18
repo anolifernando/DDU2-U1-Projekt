9d4937755475ec44bac204a49a93274df9ea3402
anolifernando <anolifernando@gmail.com>
2024-11-18 11:26:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Recommended: All functions declared here
+
+// Recommended: constants with references to existing HTML-elements
+
+const cityName = prompt("Vilken stad?");
+let cityNotFound = false; 
+
+
+for (let citiesKey)
+
+// Recommended: Ask for the city name and then the rest of the code
+
